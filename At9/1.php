ba1b2784e2dd261a0c05f32c5c04dc039ea3cfc8
Ernesto62 <72198904+Ernesto62@users.noreply.github.com>
2020-11-26 10:53:05 +0000

    Add files via upload

---
+++ At9/1.php
@@ -0,0 +1,11 @@
+<?php
+for( $i=1; $i<=20; $i++){
+	echo '<br>Aluno número' . $i;
+}
+?>
+<br>
+<?php
+for( $i=20; $i>0; $i--){
+	echo '<br>Aluno número' . $i;
+}
+?>
---
+++ At9/4.php
@@ -0,0 +1,21 @@
+<?php
+$pararLoop = 9;
+for($numero = 0; ; $numero++){
+	if ($numero==$pararLoop) {
+
+		break;
+	}
+	echo $numero."<br>";
+}
+?>
+
+<?php
+$pararLoop = 29;
+for($numero = 0; ; $numero++){
+	if ($numero==$pararLoop) {
+
+		break;
+	}
+	echo $numero."<br>";
+}
+?>
